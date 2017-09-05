fe11ef474d9dc4665e5afdb001d43c0a10aa3a6e
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-09-05 17:06:23 -0400

    pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,27 @@
+
+//Need to create a timer that counts down from 30 seconds
+//If user selects correct answer, alert "correct!"
+//Keep track of correct answers
+//Move on to next question without user input by using a timer
+//If user selects incorrect answer, alert "incorrect!" with correct answer
+//Keep track of incorrect answers
+//Move on to next question without user input by using a timer
+//Reset to new game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const quizQuestions = [
     {
         question: "Which U.S. President invented the swivel chair?",
@@ -8,4 +32,4 @@ const quizQuestions = [
         }
     correctAnswer: "c"
     }
-]
+]*/
