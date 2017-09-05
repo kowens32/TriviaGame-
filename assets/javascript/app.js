705f17b5608662e3a884bc34b39583c2ccd07598
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-09-05 16:56:12 -0400

    pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+const quizQuestions = [
+    {
+        question: "Which U.S. President invented the swivel chair?",
+        answers: {
+            a: "John Quincy Adams",
+            b: "Abraham Lincoln",
+            c: "Thomas Jefferson",
+        }
+    correctAnswer: "c"
+    }
+]
