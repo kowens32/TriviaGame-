b813be5d70941fb4404e7d5581832d63403f8988
kimberly owens <kimberlyowens@MacBook-Pro-2.local>
2017-09-06 18:24:35 -0400

    created functions for displaying questions and aswers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,8 +21,30 @@ $(document).ready(function(){
         }
     }
 
-//Each question has to be written to the DOM
-    //The answer to each question has to be pushed converted to a button by using a for loop
+    //Each question has to be written to the DOM
+    function displayQuestion(question, index) {
+        var q = $('<h2>');
+        q.attr('data-question', index)
+        q.text(question);
+        q.appendTo('.question-box')
+    }
+
+    function displayAnswers(answers) {
+        for(var i = 0; i < answers.length; i++) {
+            var b = $('<button>');
+            b.text(answers[i])
+            b.appendTo('.answers-box')
+        }
+    }
+
+    function startGame() {
+        var currentQuestion = 0;
+        displayQuestion(quizQuestions[currentQuestion].question, currentQuestion);
+        displayAnswers(quizQuestions[currentQuestion].answers);
+    }
+
+    startGame();
+
 //If user selects correct answer, write "correct!" to the DOM and show correct answer
 //Keep track of correct answers
 //Move on to next question without user input by using a timer
@@ -35,58 +57,53 @@ $(document).ready(function(){
 
 });
 
+var questionNumber = $(this).attr('data-question');
+var currentQuestion = quizQuestions[questionNumber];
+var correctAnswerIndex = currentQuestion[correctAnswer]
+var answer = currentQuestion.answers[correctAnswerIndex]
 
 
 
 
 
 
-//
-//
-//
-//
-// const quizQuestions = [
-//     {
-//         question: "Which U.S. President invented the swivel chair?",
-//         answers: {
-//             a: "John Quincy Adams",
-//             b: "Abraham Lincoln",
-//             c: "Thomas Jefferson",
-//         }
-//     correctAnswer: "c"
-//     },
-//     {
-//         question: "Which U.S. President loved bowling so much that he put a bowling alley in the White House?",
-//     answers: {
-//             a: "Richard Nixon",
-//             b: "Herbert Hoover",
-//             c: "Calvin Coolidge",
-//     },
-//
-//         correctAnswers: "a",
-//     },
-//     {
-//         question: "Which U.S. President is the only President to never be elected by the voting public?",
-//         answers: {
-//             a: "Richard Nixon",
-//             b: "Gerald R. Ford",
-//             c: "Jimmy Carter",
-//         },
-//
-//         correctAnswers: "b",
-//     },
-//
-//     {
-//         question: "Which U.S. President was terrified of the number 13?",
-//         answers: {
-//             a: "Franklin Delano Roosevelt",
-//             b: "Harry S. Truman",
-//             c: "Dwight D. Eisenhower",
-//         },
-//
-//         correctAnswers: "a",
-//     },
-//     ];
-//
-//
-//
+var quizQuestions = [
+    {
+        question: "Which U.S. President invented the swivel chair?",
+        answers: ["John Quincy Adams", "Abraham Lincoln", "Thomas Jefferson"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which U.S. President loved bowling so much that he put a bowling alley in the White House?",
+        answers: {
+                a: "Richard Nixon",
+                b: "Herbert Hoover",
+                c: "Calvin Coolidge",
+        },
+        correctAnswers: "a",
+    },
+    {
+        question: "Which U.S. President is the only President to never be elected by the voting public?",
+        answers: {
+            a: "Richard Nixon",
+            b: "Gerald R. Ford",
+            c: "Jimmy Carter",
+        },
+
+        correctAnswers: "b",
+    },
+
+    {
+        question: "Which U.S. President was terrified of the number 13?",
+        answers: {
+            a: "Franklin Delano Roosevelt",
+            b: "Harry S. Truman",
+            c: "Dwight D. Eisenhower",
+        },
+
+        correctAnswers: "a",
+    },
+    ];
+
+
+
