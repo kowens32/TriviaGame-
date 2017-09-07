ba485f750d41983be0e25ffb0110a295b8425ad7
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-09-07 12:15:38 -0400

    trying to organize variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,4 @@
 
-//Need to create a timer that counts down from 30 seconds
-var number = 31;
-var intervalId;
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-$(document).ready(function(){
-
-    intervalId = setInterval(decrement, 1000);
-
-    function decrement () {
-        number--;
-        $("#question-timer").html("<h2>" + number + "</h2>");
-
-//If number hits zero
-        if (number === 0) {
-            alert("Time up!");
-            //show correct answer
-            //Keep track of incorrect answers
-
-        }
-    }
-
-    //Each question has to be written to the DOM
-    function displayQuestion(question, index) {
-        var q = $('<h2>');
-        q.attr('data-question', index)
-        q.text(question);
-        q.appendTo('.question-box')
-    }
-
-    function displayAnswers(answers) {
-        for(var i = 0; i < answers.length; i++) {
-            var b = $('<button>');
-            b.attr('data-answers',[i]);
-            b.text(answers[i]);
-            b.appendTo('.answers-box')
-        }
-    }
-
-    function startGame() {
-        var currentQuestion = 2;
-        displayQuestion(quizQuestions[currentQuestion].question, currentQuestion);
-        displayAnswers(quizQuestions[currentQuestion].answers);
-    }
-
-    startGame();
-
-    function userSelection () {
-        var userGuess;
-        $("button").on("click", function() {
-            var userGuess = $(this).attr('data-answers');
-            console.log("what is "+userGuess);
-        });
-    }
-   userSelection();
-
-   function gameLogic() {
-
-console.log("is this" +currentQuestion);
-   }
-
-   gameLogic();
-
-
-
-
-
-//If user selects correct answer, write "correct!" to the DOM and show correct answer
-//Keep track of correct answers
-//Move on to next question without user input by using a timer
-//If user selects incorrect answer, write "incorrect!" to the DOM and show correct answer
-//Keep track of incorrect answers
-//Move on to next question without user input by using a timer
-//Reset to new game
-
-
-
-});
-
-
 var quizQuestions = [
     {
         question: "Which U.S. President invented the swivel chair?",
@@ -103,11 +23,74 @@ var quizQuestions = [
     }
 ];
 
+
+
+//Need to create a timer that counts down from 30 seconds
+var number = 31;
+var intervalId;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+
+$(document).ready(function(){
+    intervalId = setInterval(decrement, 1000);
+    function decrement () {
+        number--;
+        $("#question-timer").html("<h2>" + number + "</h2>");
+        if (number === 0) {
+            alert("Time up!");
+        }
+    }
+    decrement();
+
+
+    function displayQuestion(question, index) {
+        var q = $('<h2>');
+        q.attr('data-question', index)
+        q.text(question);
+        q.appendTo('.question-box')
+    }
+
+    function displayAnswers(answers) {
+        for(var i = 0; i < answers.length; i++) {
+            var b = $('<button>');
+            b.attr('data-answers',[i]);
+            b.text(answers[i]);
+            b.appendTo('.answers-box')
+        }
+    }
+
+    function startGame() {
+            var currentQuestion = 1;
+            displayQuestion(quizQuestions[currentQuestion].question,currentQuestion);
+            displayAnswers(quizQuestions[currentQuestion].answers);
+
+        }
+
+
+    startGame();
+
+    function userSelection () {
+        var userGuess;
+        $("button").on("click", function() {
+            var userGuess = $(this).attr('data-answers');
+            console.log("what is "+userGuess);
+        });
+    }
+   userSelection();
+
+    function gameLogic() {
+
+    }
+    gameLogic();
+
+});
+
+
 var questionNumber = $(this).attr('data-question');
 var currentQuestion = quizQuestions[questionNumber];
 var correctAnswerIndex = currentQuestion[correctAnswers];
 var correctAnswer = currentQuestion.answers[correctAnswerIndex];
-
 
 
 
