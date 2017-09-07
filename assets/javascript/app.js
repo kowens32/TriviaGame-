0c973240c217687763376f84bf6b3aff416b9575
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-09-07 16:24:43 -0400

    rightAnswer variable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-
 var quizQuestions = [
     {
         question: "Which U.S. President invented the swivel chair?",
@@ -23,26 +22,25 @@ var quizQuestions = [
     }
 ];
 
-
-
 //Need to create a timer that counts down from 30 seconds
 var number = 31;
 var intervalId;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 
+$(document).ready(function() {
 
-$(document).ready(function(){
     intervalId = setInterval(decrement, 1000);
-    function decrement () {
+
+    function decrement() {
         number--;
         $("#question-timer").html("<h2>" + number + "</h2>");
         if (number === 0) {
-            alert("Time up!");
+            // alert("Time up!");
         }
     }
-    decrement();
 
+    decrement();
 
     function displayQuestion(question, index) {
         var q = $('<h2>');
@@ -52,46 +50,42 @@ $(document).ready(function(){
     }
 
     function displayAnswers(answers) {
-        for(var i = 0; i < answers.length; i++) {
+        for (var i = 0; i < answers.length; i++) {
             var b = $('<button>');
-            b.attr('data-answers',[i]);
+            b.attr('data-answers', [i]);
             b.text(answers[i]);
             b.appendTo('.answers-box')
         }
     }
 
-    function startGame() {
-            var currentQuestion = 1;
-            displayQuestion(quizQuestions[currentQuestion].question,currentQuestion);
-            displayAnswers(quizQuestions[currentQuestion].answers);
+    for (i = 0; i <quizQuestions.length; i++) {
+        var presentQuestion = [i];
+        console.log(presentQuestion);
+    }
+        displayQuestion(quizQuestions[presentQuestion].question, presentQuestion);
+        displayAnswers(quizQuestions[presentQuestion].answers);
 
-        }
-
-
-    startGame();
+        // function correctSelection(){
+        //      bestAnswer = quizQuestions[presentQuestion].correctAnswers;
+        //     console.log('kjsdhfkjs' + bestAnswer);
+        // }
+        // correctSelection();
 
     function userSelection () {
         var userGuess;
         $("button").on("click", function() {
-            var userGuess = $(this).attr('data-answers');
-            console.log("what is "+userGuess);
-        });
-    }
-   userSelection();
+            var userGuess = $(this).attr('data-answers')
+            rightAnswer = quizQuestions[presentQuestion].correctAnswers
+            console.log('kjsdhfkjs' + bestAnswer);
+            console.log('uyest' + userGuess);
+            if (userGuess = bestAnswer)
+                alert('ciewofhdj');
+            });
 
-    function gameLogic() {
-
     }
-    gameLogic();
+    userSelection();
 
 });
-
-
-var questionNumber = $(this).attr('data-question');
-var currentQuestion = quizQuestions[questionNumber];
-var correctAnswerIndex = currentQuestion[correctAnswers];
-var correctAnswer = currentQuestion.answers[correctAnswerIndex];
-
 
 
 
